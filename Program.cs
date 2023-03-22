e4fb29e987d0c83f3ec0db9b7e1d1ff4d244ef99
Erick Fernandes <oerickfernandes@gmail.com>
2023-03-22 13:18:51 -0300

    Desáfio concluído!

--- Program.cs
+++ Program.cs
@@ -1,26 +1,34 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia: ");
+        Smartphone nokia = new Nokia(numero: "123", modelo: "Modelo C12", imei: "999999999", memoria: 128);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Whatsapp");
+        nokia.InstalarAplicativo("Facebook");
+        Console.WriteLine();
+        Console.WriteLine("Especificações:");
+        nokia.EspecificacoesFabrica();
+        Console.WriteLine("-------------------------------------------");
 
-System.Console.WriteLine("Smartphone Nokia: ");
-Smartphone nokia = new Nokia(numero: "123", modelo: "Modelo C12", imei: "999999999", memoria: 128);
-nokia.Ligar();
-nokia.ReceberLigacao();
-nokia.InstalarAplicativo("Whatsapp");
-nokia.InstalarAplicativo("Facebook");
-System.Console.WriteLine();
-System.Console.WriteLine("Especificações:");
-nokia.EspecificacoesFabrica();
-System.Console.WriteLine("-------------------------------------------");
+
+        Console.WriteLine("Smartphone Iphone: ");
+        Smartphone iphone = new Iphone(numero: "456", modelo: "Iphone SE 2020", imei: "088888888", memoria: 64);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Safari");
+        iphone.InstalarAplicativo("Telegram");
+        Console.WriteLine();
+        Console.WriteLine("Especificações:");
+        iphone.EspecificacoesFabrica();
+
+    }
+}
 
 
-System.Console.WriteLine("Smartphone Iphone: ");
-Smartphone iphone = new Iphone(numero: "456", modelo: "Iphone SE 2020", imei: "088888888", memoria: 64);
-iphone.Ligar();
-iphone.ReceberLigacao();
-iphone.InstalarAplicativo("Safari");
-iphone.InstalarAplicativo("Telegram");
-System.Console.WriteLine();
-System.Console.WriteLine("Especificações:");
-iphone.EspecificacoesFabrica();
 
